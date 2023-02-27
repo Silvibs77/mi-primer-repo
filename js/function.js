a735f80d16035aaedc3e52438031d30f078d9689
sil <silvinabsanchez1977@gmail.com>
2023-02-27 17:01:58 -0300

    mi primer commit

---
+++ js/function.js
@@ -0,0 +1,30 @@
+
+document.getElementById('principal').onclick = function(){
+    document.getElementById("nombre").innerHTML="María Perez";
+    document.getElementById("profesion").innerHTML="Profesora de Matemática";
+    document.getElementById("email").innerHTML="maria.pedraza@gmail.com";
+}    
+
+document.getElementById('info').onclick = function(){
+    document.getElementById("nombre").innerHTML="Fecha de Nacimiento 02/05/1980";
+    document.getElementById("profesion").innerHTML="Domicilio Av. Roca 1300- Lules, prov. de Tucumán";
+    document.getElementById("email").innerHTML="Teléfono 0381-153422856";
+}
+
+document.getElementById('habilidades').onclick = function(){
+    document.getElementById("nombre").innerHTML="Buena predisposición para el trabajo en equipo";
+    document.getElementById("profesion").innerHTML="Facilidad en el uso de las TICs";
+    document.getElementById("email").innerHTML="Responsabilidad en el desarrollo de cada proyecto";
+}
+
+document.getElementById('titulos').onclick = function(){
+    document.getElementById("nombre").innerHTML="Profesora de Matemática 2001";
+    document.getElementById("profesion").innerHTML="Analista de Sistemas de Información 2003";
+    document.getElementById("email").innerHTML=" ";
+}
+
+document.getElementById('cursos').onclick = function(){
+    document.getElementById("nombre").innerHTML="Enseñanza y aprendizaje de funciones -Infod- 2008 ";
+    document.getElementById("profesion").innerHTML="Resolución de problemas en el nivel secundario -FORMAR- 2015";
+    document.getElementById("email").innerHTML="Revista Digital -ICE- 2018";
+}
